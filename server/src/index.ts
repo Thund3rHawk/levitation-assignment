b7ef6d67f3aeeb3f227c1532ee1b59af718b6e79
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-10-21 14:58:22 +0530

    feat: add dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000
+app.use (cors());
 
 app.get ('/', (req,res)=>{
-    res.send ("hello world");
+    res.send ("levitation assignment backend");
 })
 
 app.listen (port, ()=>{
-    console.log(`app is listening on port: ${port}`);    
+    console.log(`app is listening on port: http://localhost:${port}`);    
 })
